113821c307c5735b1f3bcece6474bce1af20055a
sharif7761 <sharif7761@gmail.com>
2021-04-27 01:42:18 +0600

    show items if list is not empty and clear button

--- src/App.js
+++ src/App.js
@@ -45,12 +45,16 @@ function App() {
             </button>
           </div>
         </form>
-        <div className='grocery-container'>
-          <List items={list} />
-          <button className='clear-btn'>
-            clear items
-          </button>
-        </div>
+        {
+          list.length > 0 && (
+            <div className='grocery-container'>
+              <List items={list} />
+              <button className='clear-btn' onClick={() => setList([])}>
+              clear items
+              </button>
+            </div>
+        )}
+
       </section>
   )
 }
